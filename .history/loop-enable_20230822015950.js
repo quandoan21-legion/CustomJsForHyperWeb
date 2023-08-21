9f1171743263142fa636fc9da427e9c3d9a2b46a
QuanDoan <quandoan@QuanDoans-MacBook-Air.local>
2023-08-22 02:00:02 +0700

    adding youtube mobile style

---
+++ .history/loop-enable_20230822015950.js
@@ -0,0 +1,27 @@
+const buttonElement = document.getElementsByClassName("fullscreen-icon")[0];
+
+var loopButton = document.createElement("div");
+var text = document.createTextNode("loop");
+loopButton.appendChild(text);
+loopButton.setAttribute("loop", "");
+loopButton.setAttribute("id", "loop-button");
+loopButton.setAttribute("class", "yt-spec-touch-feedback-shape");
+
+var element = document.getElementsByTagName("ytm-slim-video-action-bar-renderer")[0];
+element.appendChild(loopButton);
+
+buttonElement.onclick = function () {
+    const videoElement = document.getElementID("loop-button");
+    if (videoElement.hasAttribute('loop')) {
+        videoElement.removeAttribute('loop');
+        buttonElement.innerHTML = 'Loop Video!';
+    }
+    else {
+        videoElement.setAttribute("loop", "");
+        buttonElement.innerHTML = 'Un-Loop Video!';
+    }
+
+};
+
+// const videoElement = document.getElementsByTagName("video")[0];
+// videoElement.setAttribute("loop", "");
