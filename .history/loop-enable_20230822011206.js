cc8c7750aee278e0fc8a667744440f6a4b6ff3cd
QuanDoan <quandoan@QuanDoans-MacBook-Air.local>
2023-08-22 01:12:18 +0700

    add loop button after video tag

---
+++ .history/loop-enable_20230822011206.js
@@ -0,0 +1,16 @@
+const buttonElement = document.getElementsByClassName("fullscreen-icon")[0];
+buttonElement.onclick = function() {
+   const videoElement = document.getElementsByTagName("video")[0];
+   if (videoElement.hasAttribute('loop')) {
+        videoElement.removeAttribute('loop');
+        buttonElement.innerHTML = 'Loop Video!';
+    }
+    else {
+        videoElement.setAttribute("loop", "");
+        buttonElement.innerHTML = 'Un-Loop Video!';
+    }
+ 
+};
+
+const videoElement = document.getElementsByTagName("video")[0];
+videoElement.setAttribute("loop", "");
